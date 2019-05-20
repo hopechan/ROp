79e0e3a18be87693b9731578bcb2ef1be06c6165
Roberto Morales <robertoamorales10@gmail.com>
2019-05-20 13:18:36 -0600

    controlador minimizado

--- controlador/controladorestudiante.php
+++ controlador/controladorestudiante.php
@@ -8,27 +8,7 @@ class ControladorEstudiante
         try {
             $conn = new Conexion();
 
-            $idestudiante = $t->getIdestudiante();
-
-            $nombre = $t->getNombre();
-
-            $apellido = $t->getApellidos();
-
-            $fecha_nacimiento = $t->getFechanacimiento();
-
-            $telefono = $t->getTelefono();
-
-            $email = $t->getEmail();
-
-            $direccion = $t->getDireccion();
-
-            $anio = $t->getAnio();
-
-            $seccion = $t->getSeccion();
-
-            $centroescolar = $t->getCentroescolar();
-
-            $sql = "INSERT INTO estudiante VALUES('".$idestudiante."','".$nombre."','".$apellido."','".$fecha_nacimiento."','".$telefono."','".$email."','".$direccion."','".$anio."','".$seccion."','".$centroescolar."')";
+            $sql = "INSERT INTO estudiante VALUES('".$t->getIdestudiante()."','".$t->getNombre()."','".$t->getApellidos()."','".$t->getFechanacimiento()."','".$t->getTelefono()."','".$t->getEmail()."','".$t->getDireccion()."','".$t->getAnio()."','".$t->getSeccion()."','".$t->getCentroescolar()."')";
 
             $conn->ejecutar($sql);
 
@@ -47,7 +27,7 @@ class ControladorEstudiante
 
             $sql = "SELECT * FROM estudiante";
 
-            $rs = $conn->execQueryO($sql);
+            $rs = $conn->ejecutar($sql);
 
             $ColeccionEstudiantes = array();
             //Se crea y llena un objeto Estudiante con los datos correspondientes
@@ -70,91 +50,6 @@ class ControladorEstudiante
             $conn = null;
 
             return $ColeccionPromces;
-        } catch (mysqli_sql_exception $e) {
-
-            throw new MySQLiQueryException($sql, $e->getMessage(), $e->getCode());
-        }
-    }
-    public function buscarPromce($Promce)
-    {
-
-        try {
-
-            $conn = new Conexion();
-
-            $sql = "SELECT idpromce,idestudiante,promce FROM promce WHERE promce LIKE '%".$Promce."%'";
-
-            $rs = $conn->execQueryO($sql);
-
-            $PromceEncontrado = array();
-
-            while ($Promce = $rs->fetch_assoc()) {
-
-                $t = new Promce();
-
-                $t->setIdPromce($Promce['idpromce']);
-
-                $t->setPromce($Promce['promce']);
-
-                array_push($PromceEncontrado, $t);
-            }
-            $conn = null;
-
-            return $PromceEncontrado;
-        } catch (mysqli_sql_exception $e) {
-
-            throw new MySQLiQueryException($sql, $e->getMessage(), $e->getCode());
-        }
-    }
-    public function borrarPromce($idPromce)
-    {
-        try {
-
-            $conn = new Conexion();
-
-            $id = $_GET['idpromce'];
-
-            $sql = "DELETE FROM Promce WHERE idpromce = ".$idPromce;
-
-            $conn->execQueryO($sql);
-
-            echo '<script type="text/javascript">
-     
-           alert("Se ha borrado el elemento");
-       
-         window.location="../index.php";
-          
-      </script>';
-        } catch (mysqli_sql_exception $e) {
-
-            throw new MySQLiQueryException($sql, $e->getMessage(), $e->getCode());
-        }
-    }
-
-    public function ObtenerPromcexid($idpromce)
-    {
-        try {
-            $conn = new Conexion();
-
-              $sql="SELECT idpromce, promce FROM promce WHERE idpromce =" . $idpromce;
-
-            $rs = $conn->execQueryO($sql);
-
-            $coleccion = array();
-
-            while ($Promce = $rs->fetch_assoc()) {
-
-                $t = new Promce;
-
-                $t->setIdPromce($Promce['idPromce']);
-
-                $t->setPromce($Promce['Promce']);
-
-                array_push($coleccion, $t);
-            }
-            $conn = null;
-
-            return $coleccion;
         } catch (mysqli_sql_exception $e) {
 
             throw new MySQLiQueryException($sql, $e->getMessage(), $e->getCode());
