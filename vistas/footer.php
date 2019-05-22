d6647ff9b24cd55c120f4ad11a88cc2de2be1e1e
JoseDiaz17 <Josemario833@gmail.com>
2019-05-22 14:28:03 -0600

    footer

---
+++ vistas/footer.php
@@ -0,0 +1,26 @@
+<br><br>
+<!--footer-->
+<footer class="page-footer red accent-4">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Contenido</h5>
+                <p class="grey-text text-lighten-4">:V</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Contactos</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons black-text">location_on</i> 9ª. Calle Oriente Independencia y 3ª. Av. Sur #8, Santa Ana, El Salvador</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons black-text">mail</i> info@oportunidades.org.sv</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons black-text">call</i> Tel. +503 2447 0483</a></li>
+                    <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/OportunidadFGK"><img src="https://img.icons8.com/material/24/000000/facebook.png"> Programa Oportunidades - FGK</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container right">
+            © 2019 Copyright Text
+        </div>
+    </div>
+</footer>
