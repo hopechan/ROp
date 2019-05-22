401b02238dd91eebff71f1ccb41d1a819f6b4c35
Roberto Morales <robertoamorales10@gmail.com>
2019-05-22 16:53:02 -0600

    agregar estudiante

--- vistas/agregarEstudiante.php
+++ vistas/agregarEstudiante.php
@@ -14,46 +14,55 @@
         <form method="post" class="col s12">
             <div class="row">
                 <div class="input-field col s3"> 
+                  <i class="material-icons prefix">account_circle</i>
                   <input type="text" name="txtNombre" class="validate"  id="primer_nombre"> 
                   <label for="primer_nombre">Nombre:</label>  
                 </div>
                 <div class="input-field col s3">
+                <i class="material-icons prefix">account_circle</i>
                     <input type="text" name="txtApellidos" class="validate" id="apellidos">
                     <label for="apellidos">Apellidos:</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">date_range</i>
                     <input type="date" name="Fecha" id="fecha" class="validated">
                     <label for="fecha">Fecha de Nacimiento:</label>
                 </div>
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">phone</i>
                     <input type="tel" name="telefono" id="tel" class="validated">
                     <label for="tel">Telefono:</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">mail</i>
                     <input type="email" name="email" id="mail" class="validated">
                     <label for="mail">e-mail:</label>
                 </div>
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">event</i>
                     <input type="number" name="anio" id="año" class="validated">
                     <label for="año">Año:</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6">
+                    <i class="material-icons prefix">home</i>
                     <input type="text" name="direccion" id="dir" class="validated">
                     <label for="dir">Dirección:</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">school</i>
                     <input type="text" name="txtCentroescolar" id="ce" class="validated">
                     <label for="ce">Centro Escolar:</label>
                 </div>
                 <div class="input-field col s3">
+                    <i class="material-icons prefix">group</i>
                     <input type="text" name="txtSeccion" id="seccion" class="validated">
                     <label for="seccion">Sección:</label>
                 </div>
@@ -65,7 +74,7 @@
                     </button>
                 </div>
                 <div class="input-field col s2">
-                    <button type="submit" class="btn waves-effect waves-red grey darken-3">Cancelar
+                    <button type="submit" class="btn waves-effect waves-red grey darken-3">Cancelar 
                         <i class="material-icons left">delete</i>
                     </button>
                 </div>
--- vistas/head.php
+++ vistas/head.php
@@ -6,4 +6,5 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
