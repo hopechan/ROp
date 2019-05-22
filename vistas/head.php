9e6a75982bb74e4ea8f65997df0fff9d340d4bff
Esperanza Dueñas <da13002@ues.edu.sv>
2019-05-22 14:46:49 -0600

    le cambie el titulo :v

--- vistas/head.php
+++ vistas/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Navbar</title>
+    <title>Oportunidades</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
