17aaaa15187edbace26fe41e419921b4b6d5011b
Esperanza Dueñas <da13002@ues.edu.sv>
2019-05-24 10:54:58 -0600

    agregado archivo css

---
+++ vistas/webNotas.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("head.php")?>
+<body>
+    <?php include("navbar.php")?>
+    <div class="container">
+        <div class="row">
+            <div class="tarjeta card medium col s3">
+                <div class="card-image">
+                    <img src="" alt=""/>
+                    <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div> <!-- Fin card1-->
+            <div class="tarjeta card medium col s3">
+                <div class="card-image">
+                    <img src="" alt=""/>
+                    <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div> <!-- Fin card2-->
+            <div class="tarjeta card medium col s3">
+                <div class="card-image">
+                    <img src="" alt=""/>
+                    <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">This is a link</a>
+                </div>
+            </div><!-- Fin card3-->
+        </div>
+    <!--
+        <div class="row">
+            <div class="col s7">
+                <h3>Notas Oportunidades</h3>
+            </div>
+            <br>
+            <form class="col s5">
+                <div class="col s12">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">search</i>
+                        <input id="icon_prefix" type="text" class="validate">
+                        <label for="icon_prefix">Buscar ...</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <table class="responsive-table">
+            <thead>
+                <tr>
+                    <th data-field="id">Name</th>
+                    <th data-field="name">Item Name</th>
+                    <th data-field="price">Item Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Alvin</td>
+                    <td>Eclair</td>
+                    <td>.87</td>
+                </tr>
+                <tr>
+                    <td>Alan</td>
+                    <td>Jellybean</td>
+                    <td>.76</td>
+                </tr>
+                <tr>
+                    <td>Jonathan</td>
+                    <td>Lollipop</td>
+                    <td>.00</td>
+                </tr>
+            </tbody>
+        </table>
+        -->
+    </div> <!-- Fin container-->
+</body>
+<?php include("footer.php")?>
+</html>
