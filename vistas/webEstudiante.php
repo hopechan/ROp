8da3c03f07b37c7e34cc99eb2d0bc688ea7a1ca1
Roberto Morales <robertoamorales10@gmail.com>
2019-05-27 16:01:00 -0600

    algo

--- vistas/webEstudiante.php
+++ vistas/webEstudiante.php
@@ -22,41 +22,39 @@
             <div class="col s12">
                 <div id="modal1" class="modal modal-fixed-footer tamaño">
                     <div class="modal-content center-align">
-                        <h4>Nuevo Estudiante</h4><hr>
-                        <form action="" method="post" class="col s12">
+                        <h4>Nuevo Estudiante</h4>
+                        <form action="" method="post" class="col s12" enctype="multipart/form-data">
                             <div class="row">
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">account_circle</i>
                                     <input type="text" name="txtNombre" class="validate" id="nombre">
                                     <label for="nombre">Nombre:</label>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">account_circle</i>
                                     <input type="text" name="txtApellido" class="validate" id="apellido">
                                     <label for="apellido">Apellido:</label>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">date_range</i>
                                     <input type="date" name="fecha" id="fecha" class="validate">
                                     <label for="fecha">Fecha de Nacimiento:</label>
                                 </div>
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">phone</i>
                                     <input type="tel" name="telefono" id="tel" class="validate">
                                     <label for="tel">Teléfono:</label>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">mail</i>
                                     <input type="email" name="email" id="mail" class="validate">
                                     <label for="mail">e-mail:</label>
                                 </div>
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">event</i>
                                     <input type="number" name="anio" id="anio" class="validate">
                                     <label for="anio">Año:</label>
@@ -70,12 +68,12 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">school</i>
                                     <input type="text" name="centroescolar" id="ce" class="validate">
                                     <label for="ce">Centro Escolar:</label>
                                 </div>
-                                <div class="input-field col s6">
+                                <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">group</i>
                                     <input type="text" name="seccion" id="seccion" class="validate">
                                     <label for="seccion">Sección:</label>
@@ -85,10 +83,15 @@
                         <!-- footer del formulario modal 1 -->
                     <div class="modal-footer">
                         <div class="center-align">
+                            <div class="file-field input-field boton-g btn waves-effect">
+                                    <i class="material-icons left">add_a_photo</i>
+                                    <span>Fotografía</span>
+                                    <input type="file" name="foto">
+                            </div> &nbsp;
                             <button class="btn boton-save" type="submit" name="ok">
                             <i class="material-icons left">save</i>
                             Guardar </button> &nbsp;
-                            <button class="btn boton-delete" name="nel">
+                            <button class="btn boton-delete modal-close" name="nel">
                             <i class="material-icons left">delete</i>
                             Cancelar 
                             </button>
@@ -124,6 +127,7 @@
             $ce = new controladorestudiante();
 
             #se le pasan los parametros del formulario al objeto estudiante
+            $e->setIdestudiante(null);
             $e->setNombre($nombre);
             $e->setApellidos($apellidos);
             $e->setFechanacimiento($fechanacimiento);
@@ -142,13 +146,7 @@
                     alert('Estudiante guardado con exito');
                     window.location='webEstudiante.php';
                   </script>";
-        }elseif (isset($_POST['nel'])) { #si pulsa el boton cancelar se activa un alert y regresa a la parte anterior
-            echo "<script>
-                    alert('Proceso cancelado');
-                    window.location='webEstudiante.php';
-                  </script>";
         }
-
     ?>
     <?php
     include("footer.php");
