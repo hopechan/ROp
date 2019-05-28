d454aacf1f2dfa2dc9c10cf054c1d7577643415c
Esperanza Dueñas <da13002@ues.edu.sv>
2019-05-28 10:51:27 -0600

    vista notas (semi finalizada)

--- vistas/notaCE.php
+++ vistas/notaCE.php
@@ -4,10 +4,10 @@
 <table class="striped table-responsive">
     <thead>
         <tr>
-            <th data-field="estudiante">Estudiante</th>
-            <th data-field="materia">Materia</th>
-            <th data-field="nota">Nota</th>
-            <th data-field="opt">Opciones</th>
+            <th data-field="estudiante" >Estudiante</th>
+            <th data-field="materia" >Materia</th>
+            <th data-field="nota" >Nota</th>
+            <th data-field="opt" >Opciones</th>
         </tr>
     </thead>
     <tbody>
@@ -16,11 +16,20 @@
         $notas = $cn->notasEstudiante();
         for ($i=0; $i < sizeof($notas); $i++) { 
             echo "<tr>";
-                echo "<td>".$notas[$i]->getIdestudiante()."</td>";
-                echo "<td>".$notas[$i]->getIdtipo()."</td>";
-                echo "<td>".$notas[$i]->getNota()."</td>";
+                echo "<td class=''>".$notas[$i]->getIdestudiante()."</td>";
+                echo "<td class=''>".$notas[$i]->getIdtipo()."</td>";
+                echo "<td class=''>".$notas[$i]->getNota()."</td>";
             echo "</tr>";
         }
         ?>
     </tbody>
 </table>
+<ul class="pagination center">
+  <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+  <li class="active"><a href="#!">1</a></li>
+  <li class="waves-effect"><a href="#!">2</a></li>
+  <li class="waves-effect"><a href="#!">3</a></li>
+  <li class="waves-effect"><a href="#!">4</a></li>
+  <li class="waves-effect"><a href="#!">5</a></li>
+  <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+</ul>
--- vistas/webNotas.php
+++ vistas/webNotas.php
@@ -5,15 +5,29 @@
     <?php include("navbar.php")?>
     
     <div class="container contenedor">
-        <ul id="tabsNotas" class="tabs">
-          <li class="tab col s3"><a href="#tbCE">Centro Escolar</a></li>
-          <li class="tab col s3"><a class="active" href="#tbOpor">Oportunidades</a></li>
-          <li class="tab col s3"><a href="#tbCert">Certificaciones</a></li>
-        </ul>
-        <div id="tbCE" class="col s12"><?php include_once("notaCE.php")?></div>
-        <div id="tbOpor" class="col s12"></div>
-        <div id="tbCert" class="col s12"></div>
+      <div></div>
+      <div class="row">
+          <ul id="tabsNotas" class="tabs col s8 m8">
+            <li class="tab col s3"><a class="active" href="#tbCE">Centro Escolar</a></li>
+            <li class="tab col s3"><a href="#tbOpor">Oportunidades</a></li>
+            <li class="tab col s3"><a href="#tbCert">Certificaciones</a></li>
+          </ul>
+          <div class="input-field col s4 m4">
+            <i class="material-icons prefix">search</i>
+            <input type="text" name="txtBusqueda" id="txtBusqueda" class="validate">
+            <label for="txtBusqueda">Buscar...</label>
+          </div>
+          <div id="tbCE" class=""><?php include_once("notaCE.php")?></div>
+          <div id="tbOpor" class=""></div>
+          <div id="tbCert" class=""></div>
+      </div>
     </div> <!-- Fin container-->
 </body>
 <?php include("footer.php")?>
 </html>
+
+<div class="input-field col s4 m4 l6">
+            <i class="material-icons prefix">search</i>
+            <input type="text" name="txtBusqueda" id="txtBusqueda" class="validate">
+            <label for="txtBusqueda">Buscar ...</label>
+          </div>
