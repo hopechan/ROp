ab2b8763a46e88889e8cabaae0524e7b5bfb1086
Roberto Morales <robertoamorales10@gmail.com>
2019-05-29 10:31:07 -0600

    clase size-cards

--- vistas/vistaEstudiante.php
+++ vistas/vistaEstudiante.php
@@ -17,7 +17,7 @@ include ("navbar.php");
             <div class="col s12 m12 l4">
                 <div class="card">
                     <div class="card-image waves-block">
-                        <img src="img/estudiantes.jpg" class="acativator">
+                        <img src="img/chicos.jpg" class="acativator size-cards">
                     </div>
                     <div class="card-content black">
                         <span class="card-title activator white-text">
@@ -26,8 +26,66 @@ include ("navbar.php");
                         </span>
                         <p><a href="#" class="btn grey darken-2">Visualizar</a></p>
                     </div>
-                    <div class="card-reveal">
-                        span.card-title
+                    <div class="card-reveal grey">
+                        <span class="card-title grey-text text-darken-4">
+                        <strong>
+                            Primer Año
+                        </strong>
+                        <i class="material-icons right red-text">close</i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores provident laboriosam, quisquam dicta assumenda ab ex culpa nisi sequi voluptatem expedita. Officiis modi, rerum alias ut illo voluptate fugit.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m12 l4">
+                <div class="card">
+                    <div class="card-image waves-block">
+                        <img src="img/programa.jpg" class="acativator size-cards">
+                    </div>
+                    <div class="card-content black">
+                        <span class="card-title activator white-text">
+                            Segundo Año
+                            <i class="material-icons right">more_vert</i>
+                        </span>
+                        <p><a href="#" class="btn grey darken-2">Visualizar</a></p>
+                    </div>
+                    <div class="card-reveal grey">
+                        <span class="card-title grey-text text-darken-4">
+                        <strong>
+                            Segundo Año
+                        </strong>
+                        <i class="material-icons right red-text">close</i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores provident laboriosam, quisquam dicta assumenda ab ex culpa nisi sequi voluptatem expedita. Officiis modi, rerum alias ut illo voluptate fugit.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m12 l4">
+                <div class="card">
+                    <div class="card-image waves-block">
+                        <img src="img/chicas.jpg" class="acativator size-cards">
+                    </div>
+                    <div class="card-content black">
+                        <span class="card-title activator white-text">
+                            Tercer Año
+                            <i class="material-icons right">more_vert</i>
+                        </span>
+                        <p><a href="#" class="btn grey darken-2">Visualizar</a></p>
+                    </div>
+                    <div class="card-reveal grey">
+                        <span class="card-title grey-text text-darken-4">
+                        <strong>
+                            Tercer Año
+                        </strong>
+                        <i class="material-icons right red-text">close</i>
+                        </span>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores provident laboriosam, quisquam dicta assumenda ab ex culpa nisi sequi voluptatem expedita. Officiis modi, rerum alias ut illo voluptate fugit.
+                        </p>
                     </div>
                 </div>
             </div>
