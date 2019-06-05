ee9b259424865d03dfe83dfdab34733708aae08a
Esperanza Dueñas <da13002@ues.edu.sv>
2019-06-05 15:07:38 -0600

    notas

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
     define('URL', 'http://localhost/Rop/');
     //Constantes de la conexion a la base de datos
     define('HOST', 'localhost');
-    define('DB', 'neoranking');
+    define('DB', 'neoranking2');
     define('USER', 'root');
     define('PASSWORD', '');
     define('CHARSET', 'utf8mb4');
--- views/notas/index.php
+++ views/notas/index.php
@@ -13,85 +13,59 @@
     <?php require 'views/navbar.php' ?>
     <br><br>
     <div class="container">
-        <div class="row">
-          <ul id="tabsNotas" class="tabs col s8 m8">
-            <li class="tab col s3"><a class="active" href="#tbCE">Centro Escolar</a></li>
-            <li class="tab col s3"><a href="#tbOpor">CCGK</a></li>
-            <li class="tab col s3"><a href="#tbCert">Certificaciones</a></li>
+      <div class="row">
+        <h3 class="col s8 m8">Notas</h3>
+        <br><br>
+        <div class="col s4 m4">
+          <!-- Dropdown Trigger -->
+          <a class='dropdown-trigger btn' data-target='dropdown1'>Opciones de Filtrado</a>
+          <!-- Dropdown Structure -->
+          <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="#!" >CCGK</a></li>
+            <li><a href="#!">Centro Escolar</a></li>
+            <li><a href="#!">Certificaciones</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a href="#!">Año</a></li>
+            <li><a href="#!">Seccion</a></li>
           </ul>
-          <div class="input-field col s4 m4">
-            
-          </div>
-          <!-- Tablas que consumen datos-->
-
-          <!-- Tabla Centro Escolar-->
-          <br>
-          <div id="tbCE" class="">
-            <table class="highlight table-responsive" id="tabla">
-                <thead class="black white-text">
-                    <tr>
-                        <th hidden>idNota</th>
-                        <th>Estudiante</th>
-                        <th>Materia</th>
-                        <th>Nota</th>
-                        <th colspan="2">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  <?php 
-                    require_once 'models/notas.php';
-                    foreach($this->notasCE as $item){
-                      $notaCE = new Notas();
-                      $notaCE = $item;
-                  ?>
-                  <tr>
-                      <td hidden><?php echo $notaCE->idnota; ?></td>
-                      <td><?php echo $notaCE->idestudiante; ?></td>
-                      <td><?php echo $notaCE->idmateria; ?></td>
-                      <td><?php echo $notaCE->nota?></td>
-                      <td><a href="<?php echo constant('URL') . 'nota/editar/' . $notaCE->idnota;?>" class="right btn-floating btn-large waves-effect waves-white btn-flat white-text grey darken-3 btn modal-trigger"><i class="material-icons">refresh</i></button></a></td>
-                      <td><a href="<?php echo constant('URL').'nota/eliminar/'. $notaCE->idnota;?>" class="left btn-floating btn-large waves-effect waves-black btn-flat white-text red accent-4 btn"><i class="material-icons">delete</i></a></td>
-                  </tr>
+        </div>
+      </div>
+      <div class="row">
+        <div id="tbCE" class="">
+          <table class="highlight table-responsive" id="tabla">
+            <thead class="black white-text">
+              <tr class="center-align">
+                <th hidden class="center-align">idNota</th>
+                <th>Estudiante</th>
+                <th>Materia</th>
+                <th>Nota</th>
+                <th colspan="2">Acciones</th>
+              </tr>
+              </thead>
+              <tbody>
+                <?php 
+                  require_once 'models/notas.php';
+                  foreach($this->notasCE as $item){
+                    $notaCE = new Notas();
+                    $notaCE = $item;
+                ?>
+                <tr>
+                  <td hidden><?php echo $notaCE->idnota; ?></td>
+                  <td><?php echo $notaCE->idestudiante; ?></td>
+                  <td><?php echo $notaCE->idmateria; ?></td>
+                  <td><?php echo $notaCE->nota?></td>
+                  <td><a href="<?php echo constant('URL') . 'nota/editar/' . $notaCE->idnota;?>" class="right btn-floating btn-large waves-effect waves-white btn-flat white-text grey darken-3 btn modal-trigger"><i class="material-icons">refresh</i></button></a></td>
+                  <td><a href="<?php echo constant('URL').'nota/eliminar/'. $notaCE->idnota;?>" class="left btn-floating btn-large waves-effect waves-black btn-flat white-text red accent-4 btn"><i class="material-icons">delete</i></a></td>
+                </tr>
                 <?php } ?>
-                </tbody>
+              </tbody>
             </table>
           </div>
-
-          <!--Tabla CCGK-->
-          <div id="tbOpor" class="">
-            <table class="highlight table-responsive" id="tabla">
-                <thead class="black white-text">
-                    <tr>
-                        <th hidden>idNota</th>
-                        <th>Estudiante</th>
-                        <th>Materia</th>
-                        <th>Nota</th>
-                        <th colspan="2">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  <?php 
-                    require_once 'models/notas.php';
-                    foreach($this->notasCCGK as $item){
-                      $notaCCGK = new Notas();
-                      $notaCCGK = $item;
-                  ?>
-                  <tr>
-                      <td hidden><?php echo $notaCCGK->idnota; ?></td>
-                      <td><?php echo $notaCCGK->idestudiante; ?></td>
-                      <td><?php echo $notaCCGK->idmateria; ?></td>
-                      <td><?php echo $notaCCGK->nota?></td>
-                      <td><a href="<?php echo constant('URL') . 'nota/editar/' . $tipo->idtipo;?>" class="right btn-floating btn-large waves-effect waves-white btn-flat white-text grey darken-3 btn modal-trigger"><i class="material-icons">refresh</i></button></a></td>
-                      <td><a href="<?php echo constant('URL').'nota/eliminar/'. $tipo->idtipo;?>" class="left btn-floating btn-large waves-effect waves-black btn-flat white-text red accent-4 btn"><i class="material-icons">delete</i></a></td>
-                  </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-          </div>
-
-          <!-- Tabla Certificacion-->
       </div>
     </div>
     <?php require 'views/footer.php' ?>
+    <script>
+      $('.dropdown-trigger').dropdown();
+    </script>
 </body>
 </html>
