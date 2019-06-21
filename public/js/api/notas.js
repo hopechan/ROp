12e3a03018e4cf9c24c6ce59664cb1e83ba4bff4
Esperanza Dueñas <da13002@ues.edu.sv>
2019-06-21 16:32:16 -0600

    avanze ranking

--- public/js/api/notas.js
+++ public/js/api/notas.js
@@ -2,19 +2,20 @@ import Api from './api.js';
 class Notas{
     constructor() {
         this.URL_BASE = "http://localhost/Rop/nota/";
-        var estudiantes = {};
-        var promedios = [];
-        var nombres = [];
     }
 //`\n${idestudiante} ${estudiante} ${materia} ${promedio}`
     async getNotas(tipo){
         let items = await(Api.getAll(`${this.URL_BASE}${tipo}`));
         //let datos = items.map(({idestudiante,estudiante,materia,promedio}) => ).join('');
         //const obtenerPromPorId = array => array.filter(({idestudiante}) => idestudiante == 1)
-        const datos = items.map(item => item.estudiante);
-        console.log(datos);
+        var filtros = {};
+        items.forEach( item => {
+            let filtro = filtros[item.estudiante] = filtros[item.estudiante] || {};
+        });
+        console.log(items);      
+        console.log(JSON.stringify(filtros));
+        return filtros;
     }
-
 
 }
 
