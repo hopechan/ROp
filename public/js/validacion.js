31a04e6eee43bf5742f09b63cfe9779dccaebc5b
Roberto Morales <robertoamorales10@gmail.com>
2019-06-24 14:04:31 -0600

    validacion

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,9 +1,19 @@
-window.addEventListener('load', iniciar);
-function validar()
-{
-    alert('hola');
-}
- function iniciar()
- {
-    document.getElementById('ok').addEventListener('click', validar);
- }
+$(document).ready(function(){
+    $('#formulario').submit(function(){
+        var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fecha_nacimiento = document.getElementById('fecha').value;
+    var telefono = document.getElementById('tel').value;
+    var email = document.getElementById('mail').value;
+    var anio = document.getElementById('anio').value;
+    var direccion = document.getElementById('dir').value;
+    var centro_escolar = document.getElementById('ce').value;
+    var seccion = document.getElementById('seccion').value;
+
+    if (nombre == '' || apellido == '' || fecha_nacimiento == '' || telefono == '' || email == '' || anio == '' || direccion == '' || centro_escolar == '' || seccion == '') {
+                alert('Por favor complete todos los campos');
+                return false;
+            }    
+    })
+})
+
--- views/estudiantes/index.php
+++ views/estudiantes/index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="<?php echo constant('URL')?>public/js/validacion.js"></script>
     <title>Contro de Estudiantes</title>
 </head>
 
@@ -36,59 +35,59 @@
                 <div id="modal1" class="modal modal-fixed-footer tamaño">
                     <div class="modal-content center-align">
                         <h4>Nuevo Estudiante</h4>
-                        <form action="<?php echo constant('URL');?>estudiante/insert" method="post" class="col s12" enctype="multipart/form-data">
-                            <div class="row">
-                                <div class="input-field col s12 m12 l6">
+                        <div class="row">
+                            <form action="<?php echo constant('URL');?>estudiante/insert" method="post" class="col s12" enctype="multipart/form-data" id="formulario">
+                            <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">account_circle</i>
-                                    <input type="text" name="txtNombre" class="validate" id="nombre" required>
+                                    <input type="text" name="txtNombre" class="validate" id="nombre">
                                     <label for="nombre">Nombre:</label>
                                 </div>
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">account_circle</i>
-                                    <input type="text" name="txtApellido" class="validate" id="apellido" required>
+                                    <input type="text" name="txtApellido" class="validate" id="apellido">
                                     <label for="apellido">Apellidos:</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">date_range</i>
-                                    <input type="date" name="fecha" id="fecha" class="validate" required>
+                                    <input type="date" name="fecha" id="fecha" class="validate">
                                     <label for="fecha">Fecha de Nacimiento:</label>
                                 </div>
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">phone</i>
-                                    <input type="tel" name="telefono" id="tel" class="validate" required>
+                                    <input type="tel" name="telefono" id="tel" class="validate">
                                     <label for="tel">Teléfono:</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">mail</i>
-                                    <input type="email" name="email" id="mail" class="validate" required>
+                                    <input type="email" name="email" id="mail" class="validate">
                                     <label for="mail">e-mail:</label>
                                 </div>
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">event</i>
-                                    <input type="number" name="anio" id="anio" class="validate" required>
+                                    <input type="number" name="anio" id="anio" class="validate">
                                     <label for="anio">Año:</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12">
                                     <i class="material-icons prefix">home</i>
-                                    <input type="text" name="direccion" id="dir" class="validate" required>
+                                    <input type="text" name="direccion" id="dir" class="validate">
                                     <label for="dir">Dirección:</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">school</i>
-                                    <input type="text" name="centroescolar" id="ce" class="validate" required>
+                                    <input type="text" name="centroescolar" id="ce" class="validate">
                                     <label for="ce">Centro Escolar:</label>
                                 </div>
                                 <div class="input-field col s12 m12 l6">
                                     <i class="material-icons prefix">group</i>
-                                    <input type="text" name="seccion" id="seccion" class="validate" required>
+                                    <input type="text" name="seccion" id="seccion" class="validate">
                                     <label for="seccion">Sección:</label>
                                 </div>
                             </div>
@@ -123,3 +122,4 @@
     });
     }
 </script>
+<script src="<?php echo constant('URL')?>public/js/validacion.js"></script>
