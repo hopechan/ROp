62a6716a1ed06413691f6cda7827f6d34f5c9d19
Esperanza Dueñas <da13002@ues.edu.sv>
2019-06-28 16:23:14 -0600

    plantilla super basica

--- views/estudiantes/perfil.php
+++ views/estudiantes/perfil.php
@@ -1,4 +1,19 @@
-<?php
-echo 'holi';
-echo $this->estudiante->nombre;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perfil</title>
+</head>
+<body>
+    <?php require 'views/header.php' ?>
+    <?php require 'views/navbar.php' ?>
+    <div class="container">
+        <div class="row">
+            <div class="col s3"></div>
+            <div class="col s9"></div>
+        </div>
+    </div>
+</body>
+</html>
