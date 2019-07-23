799889b565dfbee3776294200208208a9b0177cb
Esperanza Dueñas <da13002@ues.edu.sv>
2019-07-23 09:30:00 -0600

    en la vista principal se muestra solo el 10% del ranking

--- public/js/api/notas.js
+++ public/js/api/notas.js
@@ -4,14 +4,24 @@ class Notas{
         //URL_BASE = "http://localhost/Rop/";
     }
 
-    async getNotas(){
+    diezPorCiento(arreglo){
+        let filtro = Math.round(arreglo.length/10);
+        if (filtro === 0) {
+            return arreglo.length;
+        } else {
+            return filtro;
+        }
+    }
+
+    async rankingDiezPorCiento(){
         let items = await(Api.getAll(`http://localhost/Rop/nota/listaFinal`));
         let filtro = items.filter((obj, pos, arr) => {
             return arr.map(mapObj => mapObj.estudiante).indexOf(obj.estudiante) == pos;
         });
         let ranking = filtro.sort((a,b) =>(b.promedio > a.promedio) ? 1 : -1);
-        let nombres = ranking.map(items => items.estudiante);
-        let proms = ranking.map(items => items.promedio);
+        let diez = ranking.slice(0, this.diezPorCiento(ranking));
+        let nombres = diez.map(items => items.estudiante);
+        let proms = diez.map(items => items.promedio);
         let grafico = document.querySelector('#ranking');
         //lista.innerHTML = "Hola de js";
         let g = new Chart(grafico, {
@@ -41,10 +51,20 @@ class Notas{
 			var value = g.data.datasets[firstPoint._datasetIndex].data[firstPoint._index];
             if (firstPoint !== undefined){
                 let id = filtro.filter(item => item.estudiante == label).map(item => item.idestudiante);
-                http://localhost/Rop/estudiante/reportecxalumno/
+                //http://localhost/Rop/estudiante/reportecxalumno/
                 window.location = `http://localhost/Rop/estudiante/reportecxalumno/${id}`;
             }   
 		};
+    }
+
+    async rankingCienPorCiento(){
+        let items = await(Api.getAll(`http://localhost/Rop/nota/listaFinal`));
+        let filtro = items.filter((obj, pos, arr) => {
+            return arr.map(mapObj => mapObj.estudiante).indexOf(obj.estudiante) == pos;
+        });
+        let ranking = filtro.sort((a,b) =>(b.promedio > a.promedio) ? 1 : -1);
+        let nombres = ranking.map(items => items.estudiante);
+        let proms = ranking.map(items => items.promedio);
     }
 }
 export default Notas;
--- public/js/ranking.js
+++ public/js/ranking.js
@@ -2,6 +2,6 @@ import Notas from './api/notas.js';
 document.addEventListener('DOMContentLoaded', function (e) {
     e.preventDefault();
     const ranking = new Notas();
-    let data = ranking.getNotas();
+    let data = ranking.rankingDiezPorCiento();
     console.log(data);
 })
