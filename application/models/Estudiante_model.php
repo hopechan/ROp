2b044d2ffadbd336b9c48e449e89d2756b7c39e0
Esperanza Dueñas <da13002@ues.edu.sv>
2019-08-03 17:18:10 -0600

    modelos base

---
+++ application/models/Estudiante_model.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Estudiante_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Estudiante_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  public function index()
+  {
+    // 
+  }
+
+  // ------------------------------------------------------------------------
+
+}
+
+/* End of file Estudiante_model.php */
+/* Location: ./application/models/Estudiante_model.php */
---
+++ application/models/Usuario_model.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *
+ * Model Usuario_model
+ *
+ * This Model for ...
+ * 
+ * @package		CodeIgniter
+ * @category	Model
+ * @author    Setiawan Jodi <jodisetiawan@fisip-untirta.ac.id>
+ * @link      https://github.com/setdjod/myci-extension/
+ * @param     ...
+ * @return    ...
+ *
+ */
+
+class Usuario_model extends CI_Model {
+
+  // ------------------------------------------------------------------------
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  // ------------------------------------------------------------------------
+
+
+  // ------------------------------------------------------------------------
+  public function index()
+  {
+    // 
+  }
+
+  // ------------------------------------------------------------------------
+
+}
+
+/* End of file Usuario_model.php */
+/* Location: ./application/models/Usuario_model.php */
