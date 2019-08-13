66a75006aef41559b7a57eeae701a88a52d8cf25
Esperanza Duenas <da13002@ues.edu.sv>
2019-08-12 20:36:17 -0600

    modificaciones de REST

--- application/controllers/api/Example.php
+++ application/controllers/api/Example.php
@@ -1,24 +1,10 @@
 <?php
-use Restserver\Libraries\REST_Controller;
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// This can be removed if you use __autoload() in config.php OR use Modular Extensions
-/** @noinspection PhpIncludeInspection */
-//To Solve File REST_Controller not found
-require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
+include_once(APPPATH . 'libraries/REST_Controller.php');
+include_once(APPPATH . 'libraries/Format.php');
 
-/**
- * This is an example of a few basic user interaction methods you could use
- * all done with a hardcoded array
- *
- * @package         CodeIgniter
- * @subpackage      Rest Server
- * @category        Controller
- * @author          Phil Sturgeon, Chris Kacerguis
- * @license         MIT
- * @link            https://github.com/chriskacerguis/codeigniter-restserver
- */
 class Example extends REST_Controller {
 
     function __construct()
