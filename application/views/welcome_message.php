71b8d317b2b94f8ac92086be9e8fe97f73c1f1c7
JoseDiaz17 <50640072+JoseDiaz17@users.noreply.github.com>
2019-08-13 11:30:02 -0600

    super commit one

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -71,27 +71,14 @@
 <body>
 
 <div id="container">
-    <h1>Welcome to CodeIgniter!</h1>
+    <h1>Hola Raza</h1>
 
     <div id="body">
 
-        <h2><a href="<?php echo site_url('rest-server'); ?>">REST Server Tests</a></h2>
-
-        <?php if (file_exists(FCPATH.'documentation/index.html')) : ?>
-        <h2><a href="<?php echo base_url('documentation/index.html'); ?>" target="_blank">REST Server Documentation</a></h2>
-        <?php endif ?>
-
-        <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-        <p>If you would like to edit this page you'll find it located at:</p>
-        <code>application/views/welcome_message.php</code>
-
-        <p>The corresponding controller for this page is found at:</p>
-        <code>application/controllers/Welcome.php</code>
-
-        <?php if (file_exists(FCPATH.'user_guide/index.html')) : ?>
-        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="<?php echo base_url('user_guide/index.html'); ?>" target="_blank">User Guide</a>.</p>
-        <?php endif ?>
+        <h2><a href="<?php echo base_url('api/Estudiante'); ?>">Api Estudiante</a></h2>
+        <h2><a href="<?php echo base_url('api/Tipo'); ?>">Api Tipo</a></h2>
+        <h2><a href="<?php echo base_url('api/Materia'); ?>">Api Materia</a></h2>
+        <h2><a href="<?php echo base_url('api/Nota'); ?>">Api Nota</a></h2>
     </div>
 
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '' ?></p>
