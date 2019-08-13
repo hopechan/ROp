8456635f6365653d77c60bea5d9e22909f3d6279
Esperanza Dueñas <da13002@ues.edu.sv>
2019-08-13 10:24:07 -0600

    se movio el metodo buscar a Nota_Model

--- application/models/Materia_model.php
+++ application/models/Materia_model.php
@@ -30,21 +30,6 @@ class Materia_model extends CI_Model {
     return $this->db->query("SELECT m.idmateria,m.materia,t.tipo FROM tipo as t INNER JOIN materia as m WHERE t.idtipo=m.idtipo")->result();
   }
 
-  function buscar($filtro){
-    $sub_consulta = $this->db->select('idtipo')->from('tipo')->like('tipo', $filtro);
-    $this->db->select("n.idnota", "CONCAT('e.nombre, ' ', e.apellidos') as Estudiante", "m.materia", "n.nota_p1", "n.nota_p2", "n.nota_p3", "n.nota_p4")
-              ->from('nota as n')
-              ->join('estudiante as e', 'e.idestudiante = n.idestudiante', 'inner')
-              ->join('materia as m', 'm.idmateria = n.idmateria', 'inner')
-              ->where('m.idtipo', $sub_consulta, false)
-              ->or_like('m.materia', $filtro)
-              ->or_like('e.anio', $filtro)
-              ->or_like('e.seccion', $filtro)
-              ->or_like('e.apellido', $filtro)
-              ->or_like('e.nombre', $filtro)
-            ->result();
-  }
-
   function post($data){
     //inserta un nuevo registro a la base de datos
     $this->db->insert('materia', $data);
--- application/models/Nota_model.php
+++ application/models/Nota_model.php
@@ -27,7 +27,21 @@ class Nota_model extends CI_Model {
     return $this->db->query("SELECT * FROM materia WHERE idmateria = $id")->row();
   }
 
-  
+  function buscar($filtro){
+    $sub_consulta = $this->db->select('idtipo')->from('tipo')->like('tipo', $filtro);
+    $this->db->select("n.idnota", "CONCAT('e.nombre, ' ', e.apellidos') as Estudiante", "m.materia", "n.nota_p1", "n.nota_p2", "n.nota_p3", "n.nota_p4")
+              ->from('nota as n')
+              ->join('estudiante as e', 'e.idestudiante = n.idestudiante', 'inner')
+              ->join('materia as m', 'm.idmateria = n.idmateria', 'inner')
+              ->where('m.idtipo', $sub_consulta, false)
+              ->or_like('m.materia', $filtro)
+              ->or_like('e.anio', $filtro)
+              ->or_like('e.seccion', $filtro)
+              ->or_like('e.apellido', $filtro)
+              ->or_like('e.nombre', $filtro)
+            ->result();
+  }
+
 
   function post($data){
     //inserta un nuevo registro a la base de datos
