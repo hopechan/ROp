1c4e3d763fd127404e7c55160997d4427c378f5d
JoseDiaz17 <50640072+JoseDiaz17@users.noreply.github.com>
2019-08-20 14:01:51 -0600

    actualizacion

--- application/controllers/api/Tipo.php
+++ application/controllers/api/Tipo.php
@@ -6,43 +6,51 @@ include_once(APPPATH . 'libraries/Format.php');
 
 class Tipo extends REST_Controller
 {
-    
-  public function __construct(){
+  //constructor
+  public function __construct()
+  {
     parent::__construct();
     $this->load->model('Tipo_model');
   }
 
-  public function index_get($id = 0){
-    if(!empty($id)){
-        $data = $this->Tipo_model->getById($id);
-    }else{
-        $data = $this->Tipo_model->getAll();
+  //funcion para obtener los datos
+  public function index_get($id = 0)
+  {
+    if (!empty($id)) {
+      $data = $this->Tipo_model->getById($id);
+    } else {
+      $data = $this->Tipo_model->getAll();
     }
     $this->response($data, REST_Controller::HTTP_OK);
   }
 
-  function index_post(){
-    /**
-     * se crea un array y dentro del valor de cada clave se coloca
-     * la funcion $this->post('parametro')
-     * eje:
-     * {
-     *  nombre : 'juan',
-     *  apellidos: 'perez'
-     * }
-     */
+  //funcion para agregar datos
+  function index_post()
+  {
     $data = [
-        'tipo' => $this->post('tipo'),
-        'descripcion' => $this->post('descripcion')
-      ];
-    //metodo que ingresa datos en el modelo
-    $this->Tipo_model->post($data);
-    //se devuelve un mensaje y el estado ok de la peticion
-    $this->response(['Tipo Ingresado', Rest_Controller::HTTP_OK]);
+      'tipo' => $this->post('tipo'),
+      'descripcion' => $this->post('descripcion')
+    ];
+    $this->Tipo_model->post($data); //metodo que ingresa datos en el modelo
+    $this->response(['Tipo Ingresado', Rest_Controller::HTTP_OK]); //se devuelve un mensaje y el estado ok de la peticion
   }
-  function index_delete($id){
+
+  //funcion para borrar un registro
+  function index_delete($id)
+  {
     $this->Tipo_model->delete($id);
     $this->response(['Tipo eliminado', REST_Controller::HTTP_BAD_REQUEST]);
   }
 
+  //funcion para editar datos
+  function index_put()
+  {
+    $data = [
+      'idtipo' => $this->post('idtipo'),
+      'tipo' => $this->post('tipo'),
+      'descripcion' => $this->post('descripcion')
+    ];
+    $this->Tipo_model->put($data); //metodo que ingresa datos en el modelo
+    $this->response(['Tipo actualizado', Rest_Controller::HTTP_OK]); //se devuelve un mensaje y el estado ok de la peticion
+  }
 }
